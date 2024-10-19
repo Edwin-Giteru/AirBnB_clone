073e6132b6193e24cb0e92f309be080cb60e4cfb
emmanuel tindi <emmanueltindi66@gmail.com>
2024-10-19 10:16:20 +0300

    update file test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -19,7 +19,28 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.user.first_name, "")
         self.assertEqual(self.user.last_name, "")
 
-    # Add more tests specific to User model
+    def test_to_dict(self):
+        user_dict = self.user.to_dict()
+        self.assertIsInstance(user_dict, dict)
+        self.assertEqual(user_dict['__class__'], 'User')
+        self.assertIsInstance(user_dict['created_at'], str)
+        self.assertIsInstance(user_dict['updated_at'], str)
+        self.assertEqual(user_dict['email'], "")
+        self.assertEqual(user_dict['password'], "")
+        self.assertEqual(user_dict['first_name'], "")
+        self.assertEqual(user_dict['last_name'], "")
+        self.assertEqual(user_dict['id'], self.user.id)
 
+    def test_str(self):
+        string = str(self.user)
+        self.assertIn("[User]", string)
+        self.assertIn("id", string)
+        self.assertIn("created_at", string)
+        self.assertIn("updated_at", string)
+        self.assertIn("email", string)
+        self.assertIn("password", string)
+        self.assertIn("first_name", string)
+        self.assertIn("last_name", string)
+        self.assertIn("'email': ''", string)
 if __name__ == '__main__':
     unittest.main()
